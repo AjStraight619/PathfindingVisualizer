13e7165bd5c0246ae137aa70fc5e83935bc42a06
Alex Straight <compsciaj@outlook.com>
2023-08-29 19:27:15 -0700

    deleted unused files

--- src/sortingVisualizer/types/types.ts
+++
@@ -1,5 +0,0 @@
-type sortingAlgorithm = {
-  name: string; // The name of the algorithm
-  description?: string; // A short description
-  execute: (array: number[]) => number[]; // The function to execute
-};
